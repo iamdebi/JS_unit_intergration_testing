e8b24f80812745c1afcca23ab066ba8ba8d974ec
Debbie <debiurquhart@live.co.uk>
2020-01-24 14:40:53 +0000

    passes operation display test from calculation test

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -1,19 +1,27 @@
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-describe('calculator functionality', function() {
+describe("calculator functionality", function() {
   beforeEach(function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://localhost:3000');
+    browser.get("http://localhost:3000");
   });
 
   // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
-    running_total = element(by.css('#running_total'))
-    element(by.css('#number2')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
-  })
+  it("should have working number buttons", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("2");
+  });
 
+  it("should update display with arithmetical operations", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number2")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("4");
+  });
 });
