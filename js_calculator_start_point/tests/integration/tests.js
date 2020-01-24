6d4191d2857e175e6ccce3529589089fe1a84425
Debbie <debiurquhart@live.co.uk>
2020-01-24 15:19:47 +0000

    testes for a decimal number

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -48,4 +48,22 @@ describe("calculator functionality", function() {
     element(by.css("#operator_equals")).click();
     expect(running_total.getAttribute("value")).to.eventually.equal("9");
   });
+
+  it("should display a negative output", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_subtract")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("-3");
+  });
+
+  it("should display a decimal output", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_divide")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("0.4");
+  });
 });
