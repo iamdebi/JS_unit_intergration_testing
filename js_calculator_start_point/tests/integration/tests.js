406252b7081c3723cbc10fbafe7876964797503c
Debbie <debiurquhart@live.co.uk>
2020-01-24 14:46:25 +0000

    calculates running operations intergration test

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -24,4 +24,17 @@ describe("calculator functionality", function() {
     element(by.css("#operator_equals")).click();
     expect(running_total.getAttribute("value")).to.eventually.equal("4");
   });
+
+  it("should update display with multi operations", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number5")).click();
+    element(by.css("#operator_subtract")).click();
+    element(by.css("#number8")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("1");
+  });
 });
